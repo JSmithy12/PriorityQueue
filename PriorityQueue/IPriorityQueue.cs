5a02372d404fdc8be97fbc18abc9175259a87206
JSmithy12 <joshuasmith199@gmail.com>
2025-03-18 00:25:50 +0000

    Added NUnit unit tests for PriorityQueue
    Installed NUnit and NUnit3TestAdapter
    
    Added PriorityQueueTests.cs
    
    Also error fixes that slipped into previous commit

--- PriorityQueue/IPriorityQueue.cs
+++ PriorityQueue/IPriorityQueue.cs
@@ -2,7 +2,7 @@
 
 public interface IPriorityQueue<T> where T : IComparable<T>
 {
-	void Add(T, item);
+    void Add(T item, int priority); 
 	void Remove();
 	T Peek();
 	bool isEmpty();
---
+++ PriorityQueue/PriorityQueueTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using PriorityQueue;
+
+namespace PriorityQueueTests
+{
+    [TestFixture]
+    public class PriorityQueueTests
+    {
+        private UnsortedArrayPriorityQueue<string> unsortedArrayQueue;
+        private SortedLinkedPriorityQueue<string> sortedLinkedQueue;
+        private UnsortedLinkedPriorityQueue<string> unsortedLinkedQueue;
+        private HeapPriorityQueue<string> heapQueue;
+
+        [SetUp]
+        public void Setup()
+        {
+            unsortedArrayQueue = new UnsortedArrayPriorityQueue<string>(5);
+            sortedLinkedQueue = new SortedLinkedPriorityQueue<string>();
+            unsortedLinkedQueue = new UnsortedLinkedPriorityQueue<string>();
+            heapQueue = new HeapPriorityQueue<string>(5);
+        }
+
+        [Test]
+        public void Test_AddAndHead_UnsortedArrayPriorityQueue()
+        {
+            unsortedArrayQueue.Add("A", 1);
+            unsortedArrayQueue.Add("B", 2);
+            ClassicAssert.AreEqual("B", unsortedArrayQueue.Head());  // Highest priority
+        }
+
+        [Test]
+        public void Test_Remove_UnsortedArrayPriorityQueue()
+        {
+            unsortedArrayQueue.Add("A", 1);
+            unsortedArrayQueue.Add("B", 2);
+            unsortedArrayQueue.Remove();
+            ClassicAssert.AreEqual("A", unsortedArrayQueue.Head());  // Next highest priority
+        }
+
+        [Test]
+        public void Test_QueueUnderflowException()
+        {
+            Assert.Throws<QueueUnderflowException>(() => unsortedArrayQueue.Head());
+        }
+
+        [Test]
+        public void Test_QueueOverflowException()
+        {
+            unsortedArrayQueue.Add("A", 1);
+            unsortedArrayQueue.Add("B", 2);
+            unsortedArrayQueue.Add("C", 3);
+            unsortedArrayQueue.Add("D", 4);
+            unsortedArrayQueue.Add("E", 5);
+
+            Assert.Throws<QueueOverflowException>(() => unsortedArrayQueue.Add("F", 6));
+        }
+    }
+}
